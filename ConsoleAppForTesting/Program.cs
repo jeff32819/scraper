4cf9f0ce99b6bbaa260b360ddfaca2c2d5166851
Jeff Mathews <jeff32819@gmail.com>
2025-08-22 20:58:41 -0400

    Update Program.cs

--- ConsoleAppForTesting/Program.cs
+++ ConsoleAppForTesting/Program.cs
@@ -1,24 +1,16 @@
 ﻿using ScraperCode;
 
-var setup = ScraperCode.HostBuilderFactory.Create();
-
-
+var setup = HostBuilderFactory.Create();
 
 // await ConsoleAppForTesting.Test.GetFromWeb(dbSvc, "https://business.lakenonacc.org");
-//var result1 = await ConsoleAppForTesting.Test.GetFromWeb(dbSvc, "https://sebastianmoving.net");
-//var result2 = await ConsoleAppForTesting.Test.GetFromWeb(dbSvc, "https://www.sebastianmoving.net");
-
-
-//var result1 = await ConsoleAppForTesting.Test.GetFromWeb(setup.DbSvc01, "https://jeff32819.com");
-
+// var result1 = await ConsoleAppForTesting.Test.GetFromWeb(dbSvc, "https://sebastianmoving.net");
+// var result2 = await ConsoleAppForTesting.Test.GetFromWeb(dbSvc, "https://www.sebastianmoving.net");
+// var result1 = await ConsoleAppForTesting.Test.GetFromWeb(setup.DbSvc01, "https://jeff32819.com");
 
 setup.DbSvc02.DbResetWithoutWarning();
-
 await setup.DbSvc02.SeedAdd("https://jeff32819.com", "test");
-
 Console.WriteLine("START -- SCRAPING");
 await Scraper02.Process(setup.DbSvc02, setup.Logger);
 Console.WriteLine();
 Console.WriteLine("DONE -- SCRAPING");
-
 Console.WriteLine();
