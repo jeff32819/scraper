2f7b2f0146663a5243937c2fb7afb4c85030b6a3
Jeff Mathews <jeff32819@gmail.com>
2025-08-23 12:14:52 -0400

    added charsetparser

--- ConsoleAppForTesting/Program.cs
+++ ConsoleAppForTesting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ScraperCode;
+using ScraperCode.Models;
 
 var setup = HostBuilderFactory.Create();
 
@@ -8,11 +9,29 @@ var setup = HostBuilderFactory.Create();
 // var result1 = await ConsoleAppForTesting.Test.GetFromWeb(setup.DbSvc01, "https://jeff32819.com");
 
 
+var url = "https://kettlecreeksnacks.com";
 
+var handler = new HttpClientHandler
+{
+    AllowAutoRedirect = false
+};
+
+var client = new HttpClient(handler);
+client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/74.0.3729.1235");
+client.Timeout = TimeSpan.FromSeconds(30); // Set timeout to 30 seconds
+var tmp = await client.GetAsync(url);
+var xxx = new HttpClientResponse(tmp, null);
+Console.WriteLine("done1111111111111111111111");
+Console.WriteLine(await xxx.GetContentAsync());
+Console.WriteLine("done2222222222222222222222222");
+Console.ReadKey();
+return;
 
 
 setup.DbSvc02.DbReset();
-await setup.DbSvc02.SeedAdd("https://jeff32819.com", "test");
+
+
+await setup.DbSvc02.SeedAdd("https://kettlecreeksnacks.com", "test");
 Console.WriteLine("START -- SCRAPING");
 await Scraper02.Process(setup.DbSvc02, setup.Logger);
 Console.WriteLine();
--- ScraperCode/Models/HttpClientResponseContainer.cs
+++ ScraperCode/Models/HttpClientResponseContainer.cs
@@ -1,11 +1,10 @@
-﻿namespace ScraperCode.Models
+﻿namespace ScraperCode.Models;
+
+public class HttpClientResponseContainer
 {
-    public class HttpClientResponseContainer
-    {
-        public ScraperCode.Models.HttpClientResponse HttpClientResponse { get; set; }
-        public string ErrorMessage { get; set; }
-        public Uri RequestUri { get; set; }
-        public bool IsRedirected { get; set; }
-        public int StatusCode { get; set; }
-    }
+    public HttpClientResponse HttpClientResponse { get; set; }
+    public string ErrorMessage { get; set; }
+    public Uri RequestUri { get; set; }
+    public bool IsRedirected { get; set; }
+    public int StatusCode { get; set; }
 }
